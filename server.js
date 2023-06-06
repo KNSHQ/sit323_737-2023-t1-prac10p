c1a0010457d98f339091b067d7c2c2f14d9ff0b5
KNSHQ <kanishkrajvanshi@gmail.com>
2023-06-06 23:27:22 +1000

    Q

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('My Final Changes');
+	res.send('Hi my name is KNSHQ! Nice to meet you!');
 });
 
 app.listen(PORT, HOST, () => {
